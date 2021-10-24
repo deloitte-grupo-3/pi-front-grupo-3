cdba3816f6363b2a64b48cc12af688a3a08b7f62
Igor Aguiar <desenvigor@gmail.com>
2021-10-24 11:23:53 -0300

    Criação componente formulário login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
