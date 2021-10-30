d9aead33f885a31c6d911e1367d1f28f198f2287
Achiles Martins <achiles_bonne@hotmail.com>
2021-10-29 23:39:55 -0300

    Implementada autenticação de usuário e registro de novo usuário.

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import * as Feather from 'feather-icons';
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { LoginFormComponent } from '../login-form/login-form.component';
+import { ModalComponent } from '../modal/modal.component';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,11 +12,16 @@ export class NavBarComponent implements OnInit, AfterViewInit {
 
   constructor(private router: Router) { }
 
-  @ViewChild(LoginFormComponent, { static: true })
-  filho!: LoginFormComponent;
+  @ViewChild("autenticar") modalLogin!: ModalComponent;
+  @ViewChild("registro") modalRegistro!: ModalComponent;
 
   ngOnInit(): void {
+    let user: any = localStorage.getItem('user');
 
+    if (user) {
+      user = JSON.parse(user);
+      this.nomeUsuario = user.username;
+    }
   }
 
   ngAfterViewInit() {
@@ -25,6 +30,86 @@ export class NavBarComponent implements OnInit, AfterViewInit {
 
   buscar(form: any){
     this.router.navigate(["/busca"], {queryParams: {q: form.value.q}});
+  }
+
+  login?: string;
+  senha?: string;
+
+  confirmarSenha?: string;
+  senhaCadastro?: string;
+  usuarioCadastro?: string;
+  nomeUsuario?: string | undefined;
+  erroAoLogar?: boolean | undefined;
+  erroSenha?: boolean | undefined;
+  erroCadastro?: boolean | undefined;
+
+  sendForm(){
+    fetch("https://exlivraria.herokuapp.com/auth/signin", {
+      method: "post",
+      body: JSON.stringify({
+        username: this.login,
+        password: this.senha
+      }),
+      headers: {
+        "content-type": "application/json"
+      } 
+    })
+      .then(res => {
+        if (!res.ok) {
+          this.erroAoLogar = true;
+          throw new Error();
+        }
+        return res.json()
+      })
+      .then(res => {
+        localStorage.setItem('user', JSON.stringify(res));
+        this.modalLogin.fecharModal();
+        this.nomeUsuario = this.login;
+        setTimeout(() => Feather.replace(), 0);
+      })
+  }
+
+  registrarUsuario(){
+
+    if (this.senhaCadastro !== this.confirmarSenha) {
+      this.erroSenha = true;
+      return;
+    }
+
+    fetch("https://exlivraria.herokuapp.com/auth/createUser", {
+      method: "post",
+      body: JSON.stringify({
+        username: this.usuarioCadastro,
+        password: this.senhaCadastro
+      }),
+      headers: {
+        "content-type": "application/json"
+      } 
+    })
+      .then(res => {
+        if (!res.ok) {
+          this.erroCadastro = true;
+          throw new Error();
+        }
+        return res.json()
+      })
+      .then(res => {
+        localStorage.setItem('user', JSON.stringify(res));
+        this.modalRegistro.fecharModal();
+        this.nomeUsuario = this.usuarioCadastro;
+        setTimeout(() => Feather.replace(), 0);
+      })
+  }
+
+  abrirRegistro() {
+    this.modalLogin.fecharModal();
+    this.modalRegistro.abrirModal();
+  }
+
+  sair(){
+    localStorage.removeItem('user');
+    this.nomeUsuario = undefined;
+    setTimeout(() => Feather.replace(), 0);
   }
 
 }
